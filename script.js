798d6e95427a3a32ad45cebf3d9831fc1021d6b6
J-Carlos99 <87335255+J-Carlos99@users.noreply.github.com>
2023-03-01 22:32:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var valorDoBitcoin = 1;
+var cotacaoDoBitcoin = 23805.6;
+var valorEmDolar = valorDoBitcoin * cotacaoDoBitcoin;
+valorEmDolar = valorEmDolar.toFixed(2);
+var nome = "José Carlos";
+alert("olá, " + nome + " o valor em Doler é U$" + valorEmDolar);
